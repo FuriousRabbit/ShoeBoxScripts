a6d1f25624337ecb5daee1b41ad143e6342684d1
Robbie <rreese@milkyway.localdomain>
2013-10-12 10:24:43 -0400

    Changes to fincause.py

--- Python/findcause.py
+++ Python/findcause.py
@@ -7,7 +7,7 @@ import subprocess
 import string
 import pprint
 if len(sys.argv) == 1:
-    print "\n\nUsage: findcause.py [logfile] [start_time] [end_time]"
+    print "\nUsage: findcause.py [logfile] [start_time] [end_time]"
     print "Example: findcause.py /var/log/sa/sa04 10:00:00 14:30:00\n"
 else:
     #get the required input
@@ -40,19 +40,60 @@ else:
         x = input
         if x == "cpuidle":
             z = "$9"
-        if x == "date":
-	    z = "$1"
-        cpuidle = "/usr/bin/sar -f %s -s %s -e %s | awk '{ print %s }' | grep -v 'idle' | grep -v 'Average' | grep -v 'Linux'"% (logfile_path, start_time, end_time, z)
+	    y = "-u"
+        if x == "idledate":
+	    z = "$1, $2"
+	    y = "-u"
+	if x == "load":
+            z = "$5, $6, $7"
+            y = "-q"
+	if x == "loaddate":
+	    z = "$1, $2"
+            y = "-q"
+        cpuidle = "/usr/bin/sar -f %s -s %s -e %s %s | awk '{ print %s }' | grep -v 'idle' | grep -v 'Average' | grep -v 'Linux'"% (logfile_path, start_time, end_time, y, z)
         sar = subprocess.Popen(cpuidle,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
         output = sar.communicate()[0]
         idle = string.split(output[1:-1], '\n')
         return idle
     #join our date and cpuidle list information together; highlighting the lowest idle
     cpuidle = sar_interface("cpuidle")[1:-1]
-    date = sar_interface("date")[1:]
-    busy = min(cpuidle)
+    busycpu = min(cpuidle)
+    date = sar_interface("idledate")[1:]
+    load = sar_interface("load")[3:-1]
+    busyload = max(load)
+    loaddate = sar_interface("loaddate")[1:]
     for n,i in enumerate(cpuidle):
-        if i==busy:
-	    cpuidle[n]='\033[93m' + busy + '\033[0m'
-    for x,y in zip(date, cpuidle):
-     	print '%s      %s'% (x, y) 
+        if i==busycpu:
+	    cpuidle[n]='\033[93m' + busycpu + '\033[0m'
+    for n,i in enumerate(load):
+        if i==busyload:
+            load[n]='\033[93m' + busyload + '\033[0m'
+    #print "\nCPU Idle Percentages:"
+    #for x,y in zip(date, cpuidle):
+    # 	print '%s      %s '% (x, y)
+    #print "\nLoad Averages:"
+    #for n,i in enumerate(load):
+    #    if i==busyload:
+    #        load[n]='\033[93m' + busyload + '\033[0m'
+    #for x,y in zip(loaddate, load):
+    #    print '%s      %s '% (x, y)
+    print "\nCPU Idle Percent         Load Averages"
+    print "__________________       ___________________________"
+    for a,b,c,d in zip(date, cpuidle, loaddate, load):
+	print '%s  %s       %s  %s'% (a, b, c, d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
